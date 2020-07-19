40b6f9b6d1023abf8314306734643a5029cac3fd
Paul <isakura313@gmail.com>
2020-07-19 12:36:02 +0300

    adsasd

---
+++ blog/blog_part/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'blog_part'
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<slug:post>', views.post_detail, name = 'post_detail'),
+] 
+              # +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
